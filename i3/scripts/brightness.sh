08eaf3f12f400aa1b227f94d812e423ac3ebb166
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2019-11-29 00:15:51 +0530

    min brightness

---
+++ i3/scripts/brightness.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -n "$1" ]; then
+    if [ "$1" == "-d" ]; then
+        if [ $(echo "$(light -G) > 10" | bc) -ne 0 ]; then
+            light -U 10 && dialog.sh " "
+        else 
+            notify-send "Brightness minimum"
+        fi
+    elif [ "$1" == "-i" ]; then
+        light -A 10 && dialog.sh " "
+    else
+        echo "incorrect flag"
+    fi
+else 
+    echo "use '-i' to increase brightness & '-d' to decrease brightness"
+fi
