3d3bd7795c7491e6fd8985ddf4f8fa52cf3e27ce
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2019-11-28 20:30:35 +0530

    PrtScr with ImageMagick import

--- i3/dialog.sh
+++ i3/dialog.sh
@@ -2,10 +2,10 @@
 
 DIALOG=${DIALOG=Xdialog}
 
-if [$1 = ""] ; then
+if [ -z "$1" ] ; then
     echo "NO message provided"
 else
-    echo "YOU provided the arguments" "$1"
+    echo "Message: " "$1"
     $DIALOG --infobox "$1" 0 0
 
 fi
