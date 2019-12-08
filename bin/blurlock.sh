d12dc8bda6affcf221d81861db41dcba7b0a719b
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2019-12-08 19:24:51 +0530

    updates blurlock.sh

--- bin/blurlock.sh
+++ bin/blurlock.sh
@@ -3,11 +3,22 @@
 # take screenshot
 import -window root /tmp/screenshot.png
 
-# blur it
-convert /tmp/screenshot.png -blur 0x5 \
-    "$HOME/.config/i3/token.png" -gravity center \
-    -composite /tmp/screenshotblur.png
-rm /tmp/screenshot.png
+case "$1" in
+    "")
+        # blur it 
+        convert /tmp/screenshot.png -blur 0x5 \
+            "$HOME/.config/i3/token.png" -gravity center \
+            -composite /tmp/screenshotblur.png
+        rm /tmp/screenshot.png
+        ;;
+    -d)
+        # blur it
+        convert /tmp/screenshot.png -scale 10% -scale 1000% \
+            "$HOME/.config/i3/token.png" -gravity center \
+            -composite /tmp/screenshotblur.png
+        rm /tmp/screenshot.png
+        ;;
+esac;
 
 # lock the screen
 i3lock -fe -i /tmp/screenshotblur.png
