28226fdca2e8867398ba78fa600f6cd38b4219df
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2020-03-20 02:13:08 +0530

    status update

--- bin/dwm_status.sh
+++ bin/dwm_status.sh
@@ -4,24 +4,35 @@ print_wifi() {
     # wlp=$(grep "^\s*w" /proc/net/wireless | awk '{print $1}')
 
     # if [ $wlp == "wlp6s0:" ]; then
-    #     echo " "
+    #     echo 
     # else
-    #     echo "" 
+    #     echo 
     # fi
    nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -c5- 
 }
 
+print_batstatus() {
+    status=$(acpi -b | awk '{print $3}')
+    case $status in
+        "Charging,")
+            echo  
+            ;;
+        "Discharging,")
+            echo  
+            ;;
+        *)
+    esac
+}
 print_bat() {
-    acpi -b | sed -ne 's/^.*ging,//p' | cut -c1-4
+    acpi -b | sed -ne 's/^.*ging,//p' | cut -c2-4
 }
 
 print_date() {
-    date "+%a %d, %R" 
+    date "+%a %d, %H:%M:%S" 
 }
 
 while true; do
-    xsetroot -name " $(print_wifi) $(print_bat) $(print_date)"
-    sleep 1m   # Update time every minute
+    xsetroot -name " $(print_wifi)  $(print_batstatus) $(print_bat)  $(print_date)"
+    sleep 1   # Update time every minute
 done &
-
 
