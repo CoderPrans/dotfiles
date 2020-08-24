b54edce4df10830f1dc0dfafc473ef7bad3d5847
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2020-08-25 00:02:31 +0530

    dwm autostart

---
+++ .dwm/autostart.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#urxvtd &
+compton &
+nm-applet &
+blueman-applet &
+xss-lock -- blurlock.sh -d &
+feh --bg-center "$(cat ~/.local/mybg)" &
+#nitrogen --restore &
+
+# Statusbar loop
+# while true; do
+#     xsetroot -name " $(acpi -b | sed -ne 's/^.*ging,//p' | cut -c1-4) $(date +" %a %d, %R" )"
+#    sleep 1m   # Update time every minute
+# done &
+
+dwm_status.sh &
+
