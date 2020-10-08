139f577c80981008573aa267bea56738abe8231c
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2020-10-08 11:54:05 +0530

    plank & updates

--- .config/polybar/network.sh
+++ .config/polybar/network.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
 if [ "$(cat /sys/class/net/w*/operstate)" = 'up' ]; then
-    echo "$(grep "^\s*w" /proc/net/wireless | awk '{ print "   ", int($3 * 100 / 70) "%" }')"
-fi
-
-if [ "$(cat /sys/class/net/e*/operstate)" = 'up' ]; then
-    echo "$(sed "s/down//;s/up//" /sys/class/net/e*/operstate)"
+    echo "$(grep "^\s*w" /proc/net/wireless | awk '{ print int($3 * 100 / 70) "%" }')"
+elif [ "$(cat /sys/class/net/e*/operstate)" != 'down' ]; then
+    echo "$(sed "s/down//;s/unknown//;s/up//" /sys/class/net/e*/operstate)"
+else
+    echo "" 
 fi
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,23 +1,11 @@
-; install packages
-(package-initialize)
-(require 'package)
-
-(eval-when-compile
-  (require 'use-package))
-
-;(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'load-path "~/.emacs.d/load/")
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'default-frame-alist '(font . "Hack Nerd Font-10"))
-
-; essential settings  
+;essential settings  
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(fringe-mode -1)
-(global-display-line-numbers-mode)
-(setq display-line-numbers-type t)
+;(fringe-mode -1) 
+;(global-display-line-numbers-mode)
+;(setq display-line-numbers-type t)
+(setq backup-directory-alist `(("." . ".emacs")))
 (ido-mode 1)
 (show-paren-mode 1)
 (column-number-mode 1)
@@ -25,13 +13,29 @@
 (global-hl-line-mode 1)
 
 ; enable transient mark mode
-(transient-mark-mode 1)
+;(transient-mark-mode 1)
 ;; org mode configuration
 ;; enable org mode
 (require 'org)
-;; make org mode work with files ending in .org
-;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
+; require and initialize packages
+(require 'package)
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/load/")
+(add-to-list 'package-archives
+  '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+  '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
+
+; require use-package
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;(eval-when-compile
+;  (require 'use-package))
 
 ;evil mode
 (use-package evil
@@ -39,14 +43,19 @@
   :config
   (evil-mode 1))
 
+(define-key evil-ex-map "e" 'find-file)
+
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
 (use-package helm
   :ensure t)
 
-(use-package ir-black-theme
+(use-package cyberpunk-theme
   :ensure t)
 
-(load-theme 'ir-black t)
-(define-key evil-ex-map "e" 'find-file)
+(load-theme 'cyberpunk t)
 
 (use-package helm
   :init
@@ -54,31 +63,45 @@
     (setq helm-split-window-in-side-p t
           helm-move-to-line-cycle-in-source t)
   :config 
-    (helm-mode 1) ;; Most of Emacs prompts become helm-enabled
-    (helm-autoresize-mode 1) ;; Helm resizes according to the number of candidates
-    (global-set-key (kbd "C-x b") 'helm-buffers-list) ;; List buffers ( Emacs way )
-    (define-key evil-ex-map "l" 'helm-buffers-list) ;; List buffers ( Vim way )
-    (global-set-key (kbd "C-x r b") 'helm-bookmarks) ;; Bookmarks menu
-    (global-set-key (kbd "C-x C-f") 'helm-find-files) ;; Finding files with Helm
-    (define-key evil-ex-map "e" 'helm-find-files) ;; List buffers ( Vim way )
-    (global-set-key (kbd "M-c") 'helm-calcul-expression) ;; Use Helm for calculations
-    (global-set-key (kbd "C-s") 'helm-occur)  ;; Replaces the default isearch keybinding
-    (global-set-key (kbd "C-h a") 'helm-apropos)  ;; Helmized apropos interface
-    (global-set-key (kbd "M-x") 'helm-M-x)  ;; Improved M-x menu
-    (global-set-key (kbd "M-y") 'helm-show-kill-ring)  ;; Show kill ring, pick something to paste
+    (helm-mode 1) ;; prompts become helm-enabled
+    (helm-autoresize-mode 1) ;;resizes
+    (global-set-key (kbd "C-x b") 'helm-buffers-list)
+    (define-key evil-ex-map "l" 'helm-buffers-list)
+    (global-set-key (kbd "C-x r b") 'helm-bookmarks)
+    (global-set-key (kbd "C-x C-f") 'helm-find-files)
+    (define-key evil-ex-map "e" 'helm-find-files)
+    (global-set-key (kbd "M-c") 'helm-calcul-expression)
+    (global-set-key (kbd "C-s") 'helm-occur)
+    (global-set-key (kbd "C-h a") 'helm-apropos)
+    (global-set-key (kbd "M-x") 'helm-M-x)  ;; M-x menu
+    (global-set-key (kbd "M-y") 'helm-show-kill-ring)
   :ensure t)
 
+
+(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+
+;(use-package doom-modeline
+;  :ensure t
+;  :init (doom-modeline-mode 1))
+
+
+;(require 'exwm)
+;(require 'exwm-config)
+;(exwm-config-default)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(use-package key-chord ir-black-theme helm gruvbox-theme flatland-theme flatland-black-theme evil danneskjold-theme ample-theme)))
+ '(ansi-color-names-vector
+   ["#000000" "#8b0000" "#00ff00" "#ffa500" "#7b68ee" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(custom-safe-themes
+   '("830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" default))
+ '(fci-rule-color "#383838"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:extend t :background "#222" :underline nil)))))
+ )
