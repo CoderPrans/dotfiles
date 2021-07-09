b1d27b7a93ea9ec13448d60d8bfdb5a38e678d7a
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2021-07-09 11:18:22 +0530

    init file updates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,50 +1,67 @@
 
-;; Essentials
+					; Defaults
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
-(global-display-line-numbers-mode)
 (show-paren-mode 1)
+(scroll-bar-mode -1)
 (column-number-mode 1)
-(global-hl-line-mode 1)
-(setq ring-bell-function 'ignore)
+;; (global-hl-line-mode 1)
+;; (global-display-line-numbers-mode)
+(set-default
+ 'indicate-empty-lines t)
 
-;; IDO
+(setq initial-frame-alist
+      '((top . 0) (left . 0)
+	(width . 85) (height . 32)))
+(setq ring-bell-function 'ignore)
+(setq inhibit-startup-screen t)
+(setq tab-always-indent 'complete)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+
+					; IDO
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 
-;; Customized backup
+
+					; Customized backup
 (setq auto-save-default nil)
 (setq create-lockfiles nil)
 (setq backup-directory-alist `(("." . "~/.emacs.d/backup")))
 
+
+					; Package repos
 (require 'package)
 (package-initialize)
 
-;; Add package repos
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+	     '("melpa" .
+	       "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+	     '("melpa-stable" .
+	       "https://stable.melpa.org/packages/") t)
+
 (add-to-list 'load-path "~/.emacs.d/load/")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/load")
 
-;; Install packages
-(defvar my-packages '(ag
-		      evil
-		      cider
-		      magit
-		      paredit
-		      prettier
-		      web-mode
-		      flycheck
-		      projectile
-		      python-mode
-		      cyberpunk-theme
-		      typescript-mode
-		      exec-path-from-shell
-		      ))
+
+					; Packages
+(defvar my-packages
+  '(ag
+    evil
+    cider
+    magit
+    winum
+    paredit
+    prettier
+    flycheck
+    projectile
+    smartparens
+    cyberpunk-theme
+    typescript-mode
+    exec-path-from-shell
+    ))
 
 (dolist (p my-packages)
   (unless (package-installed-p p)
@@ -52,57 +69,82 @@
     (package-install p))
   (add-to-list 'package-selected-packages p))
 
-;; ;; shell path
+;; shell path
 ;; (exec-path-from-shell-initialize)
 
-;; Js/Tsx modes
-(add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 
-;; Web Mode
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+					; Syntax
+(add-to-list 'auto-mode-alist
+	     '("\\.jsx\\'" . javascript-mode))
+(add-to-list 'auto-mode-alist
+	     '("\\.tsx\\'" . typescript-mode))
 
-;; Evil
+
+					; Evil
 (evil-mode 1)
-(setq evil-move-beyond-eol t)
+(setq evil-move-cursor-back nil
+      evil-move-beyond-eol t
+      evil-want-fine-undo t
+      evil-mode-line-format 'before
+      evil-normal-state-cursor '(box "orange")
+      evil-emacs-state-cursor  '(box "purple"))
 
-;; Projectile
+
+					; SmartParens
+(require 'smartparens-config)
+(add-hook 'after-init-hook #'smartparens-global-mode)
+(global-set-key (kbd "C->") 'sp-forward-slurp-sexp)
+(global-set-key (kbd "C-<") 'sp-forward-barf-sexp)
+
+
+					; Projectile
 (projectile-mode +1)
-(define-key projectile-mode-map (kbd "C-,") 'projectile-command-map)
+(setq projectile-create-missing-test-files t)
+(define-key projectile-mode-map
+  (kbd "C-c p") 'projectile-command-map)
 
-;; Flycheck
-(add-hook 'after-init-hook #'global-flycheck-mode)
-; workaround for slow eslint --print-config
+
+					; Flycheck
+(add-hook 'after-init-hook
+	  #'global-flycheck-mode)
+;; workaround for slow eslint --print-config
 (with-eval-after-load 'flycheck
-  (advice-add 'flycheck-eslint-config-exists-p :override (lambda() t)))
+  (advice-add 'flycheck-eslint-config-exists-p
+	      :override (lambda() t)))
 
-;; Prettier
-(add-hook 'after-init-hook #'global-prettier-mode)
 
-;; Appearance
+					; Prettier
+;; (add-hook 'after-init-hook
+;; 	  #'global-prettier-mode)
+
+
+					; Appearance
 (load-theme 'cyberpunk t)
-(add-to-list 'default-frame-alist '(font . "Hack-10"))
+;; (add-to-list 'default-frame-alist
+;; 	     '(font . "Hack-10"))
 
-;; Winner mode
+
+					; Winum mode
+(winum-mode 1)
+(global-set-key (kbd "C-c 1") 'winum-select-window-1)
+(global-set-key (kbd "C-c 2") 'winum-select-window-2)
+(global-set-key (kbd "C-c 3") 'winum-select-window-3)
+(global-set-key (kbd "C-c 4") 'winum-select-window-4)
+(global-set-key (kbd "C-c 5") 'winum-select-window-5)
+(global-set-key (kbd "C-c 6") 'winum-select-window-6)
+
+
+					; Winner mode
 (winner-mode 1)
 (global-set-key (kbd "C-c C-h") 'winner-undo)
 (global-set-key (kbd "C-c C-l") 'winner-redo)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" default))
- '(fci-rule-color "#383838")
- '(package-selected-packages
-   '(nov flycheck org-present epresent python-mode web-mode magit cyberpunk-theme projectile ag paredit cider evil use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+					; More bindings
+(defun open-init-el ()
+  (interactive)
+  (find-file (expand-file-name "init.el" user-emacs-directory)))
+
+(global-set-key (kbd "C-c i") 'open-init-el)
+(global-set-key (kbd "C-c k") 'kill-this-buffer)
+
