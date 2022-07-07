42db3a54ba56455a38037c1ead3e2103760dc51e
Pranav <pranavbhaskar17@gmail.com>
2022-07-08 01:56:09 +0530

    Emacs eslint, prettier

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,14 +11,14 @@
 ;; (global-display-line-numbers-mode)
 (setq initial-frame-alist
       '((top . 0) (left . 0)
-	(width . 85) (height . 32)))
+	(width . 105) (height . 32)))
 (setq ring-bell-function 'ignore)
 (setq inhibit-startup-screen t)
 (setq tab-always-indent 'complete)
 (defalias 'yes-or-no-p 'y-or-n-p)
 ;; set transparency
-(set-frame-parameter (selected-frame) 'alpha '(95 95))
-(add-to-list 'default-frame-alist '(alpha 95 95))
+(set-frame-parameter (selected-frame) 'alpha '(90 90))
+(add-to-list 'default-frame-alist '(alpha 90 90))
 
 
 
@@ -58,6 +58,7 @@
 		     geiser-chicken
 		     cyberpunk-theme
 		     typescript-mode
+		     exec-path-from-shell
 		     )))
   (dolist (p my-packages)
     (unless (package-installed-p p)
@@ -120,15 +121,32 @@
 (define-key projectile-mode-map
   (kbd "C-c p") 'projectile-command-map)
 
+					; Exec Path From Shell
+
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
 
 					; Flycheck
-;; (add-hook 'after-init-hook
-;; 	  #'global-flycheck-mode)
-;; ;; workaround for slow eslint --print-config
-;; (with-eval-after-load 'flycheck
-;;   (advice-add 'flycheck-eslint-config-exists-p
-;; 	      :override (lambda() t)))
+(add-hook 'after-init-hook
+	  #'global-flycheck-mode)
+;; workaround for slow eslint --print-config
+(with-eval-after-load 'flycheck
+  (advice-add 'flycheck-eslint-config-exists-p
+	      :override (lambda() t)))
 
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint
+          (and root
+               (expand-file-name "node_modules/.bin/eslint"
+                                 root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
 					; Syntax
 (add-to-list 'auto-mode-alist '("\\.js\\'"    . rjsx-mode))
@@ -139,8 +157,8 @@
 
 
 					; Prettier
-;; (add-hook 'after-init-hook
-;; 	  #'global-prettier-mode)
+
+(add-hook 'rjsx-mode-hook #'prettier-mode)
 
 
 					; Appearance
@@ -159,7 +177,7 @@
 ;;  '(mode-line ((nil (:foreground "Black" :background "DarkGray")))))
 ;; (enable-theme 'default-black)
 (add-to-list 'default-frame-alist
-             '(font . "CascadiaMono-10"))
+             '(font . "Cascadia Mono PL-10"))
 (load-theme 'cyberpunk t)
 
 
